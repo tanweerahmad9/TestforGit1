95984ddef2ece23d610332d8c9a8a27dc76bb07a
tanweerahmad9 <140061768+tanweerahmad9@users.noreply.github.com>
2023-07-24 15:59:49 +0530

    Update Program.cs
    Only one messages is enough . stay with this code .

--- TestforGit1/Program.cs
+++ TestforGit1/Program.cs
@@ -4,16 +4,3 @@ using System;
 Console.WriteLine("Hello, Tanweer !");
 Console.WriteLine("whats up are you ready to learn github!");
 console.writeLine("yes!");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Tanweer !");
-Console.WriteLine("whats up are you ready to learn github!");
-console.writeLine("yes!");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Tanweer !");
-Console.WriteLine("whats up are you ready to learn github!");
-console.writeLine("yes!");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Tanweer !");
-Console.WriteLine("whats up are you ready to learn github!");
-console.writeLine("yes!");
-
