7843b05e7f2e19142a39ec0e81e9abbc4b10e9b6
tanweerahmad9 <140061768+tanweerahmad9@users.noreply.github.com>
2023-07-24 15:57:43 +0530

    Now it is many masseges.

--- TestforGit1/Program.cs
+++ TestforGit1/Program.cs
@@ -1,4 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, Tanweer !");
 Console.WriteLine("whats up are you ready to learn github!");
 console.writeLine("yes!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Tanweer !");
+Console.WriteLine("whats up are you ready to learn github!");
+console.writeLine("yes!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Tanweer !");
+Console.WriteLine("whats up are you ready to learn github!");
+console.writeLine("yes!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Tanweer !");
+Console.WriteLine("whats up are you ready to learn github!");
+console.writeLine("yes!");
+
