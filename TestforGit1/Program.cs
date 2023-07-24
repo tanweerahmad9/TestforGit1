cee52c3cdfe974ddccb47ef4d27dbdf39bc3e633
tanweerahmad9 <140061768+tanweerahmad9@users.noreply.github.com>
2023-07-24 16:03:39 +0530

    Revert "Update Program.cs"
    This reverts commit 95984ddef2ece23d610332d8c9a8a27dc76bb07a.

--- TestforGit1/Program.cs
+++ TestforGit1/Program.cs
@@ -4,3 +4,16 @@ using System;
 Console.WriteLine("Hello, Tanweer !");
 Console.WriteLine("whats up are you ready to learn github!");
 console.writeLine("yes!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Tanweer !");
+Console.WriteLine("whats up are you ready to learn github!");
+console.writeLine("yes!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Tanweer !");
+Console.WriteLine("whats up are you ready to learn github!");
+console.writeLine("yes!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Tanweer !");
+Console.WriteLine("whats up are you ready to learn github!");
+console.writeLine("yes!");
+
